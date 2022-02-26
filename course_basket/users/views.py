5d6a7b2a81874e6c65e181ecec2d10302738c186
unknown <avni.mittal2002@gmail.com>
2022-02-26 16:56:04 +0530

    login logout function added

---
+++ course_basket/users/views.py
@@ -0,0 +1,26 @@
+from pickletools import read_uint1
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def register(request):
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}, You can now Login')
+            return redirect('login')
+
+    else:
+        form = UserRegistrationForm()
+    
+    return render (request, 'users/register.html', {'form':form } )
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
